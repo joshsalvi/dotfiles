e142cd3528b5a82b2cb36a649d350b7c9a4a01be
Tim Otlik <t.otlik@vivaldi.net>
2018-02-16 01:32:18 +0100

    fix editor to work with zsh, add NVM install installs current node release installs npm with commitizen and cz-conventional changelog uses wget or curl depending on the installed binaries adds autocompletion for venv command

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-#! /bin/bash
-
 files=( ".zsh_addons" ".zshrc" ".nanorc" ".powerlevelrc" )
 bins=( "editor" "nowplaying" )
+completions=( "_venv" )
 
 
+mkdir -p $HOME/.dotfiles/bin $HOME/.dotfiles/backup $HOME/.dotfiles/.comp
 
-mkdir -p $HOME/.dotfiles/bin $HOME/.dotfiles/backup
+echo "Cloning and installing .nanorc files from github..."
+git clone https://github.com/nanorc/nanorc nanofiles
+cd nanofiles
+make install
+cd ..
+rm -rf nanofiles
+
 
 echo "Copying files..."
 for file in ${files[@]}; do
@@ -22,23 +28,44 @@ for bin in ${bins[@]}; do
   chmod +x $HOME/.dotfiles/bin/$bin
 done
 
-echo "Cloning and installing .nanorc files from github..."
-git clone https://github.com/nanorc/nanorc nanofiles
-cd nanofiles
-make install
-cd ..
-rm -rf nanofiles
-
-echo "Downloading antigen for zsh..."
-curl -L git.io/antigen > antigen.zsh && mv antigen.zsh $HOME/.dotfiles/bin/antigen.zsh
-
+echo "Copying completions..."
+for completion in ${completions[@]}; do
+  cp .comp/$completion $HOME/.dotfiles/.comp/$completion
+  chmod +x $HOME/.dotfiles/.comp/$completion
+done
 
 echo "Installing dependencies..."
+
 pip3 install pygments --user
 if [ "$(uname)" = "Linux" ]; then
-wget -O $HOME/.dotfiles/bin/sp https://gist.githubusercontent.com/wandernauta/6800547/raw/2c2ad0f3849b1b1cd1116b80718d986f1c1e7966/sp
-chmod +x $HOME/.dotfiles/bin/sp
+  wget -O $HOME/.dotfiles/bin/sp https://gist.githubusercontent.com/wandernauta/6800547/raw/2c2ad0f3849b1b1cd1116b80718d986f1c1e7966/sp
+  chmod +x $HOME/.dotfiles/bin/sp
+
+  mkdir ~/local
+  mkdir ~/node-latest-install
+  cd ~/node-latest-install
+  curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+  ./configure --prefix=~/local
+  make install # ok, fine, this step probably takes more than 30 seconds...
+  curl https://www.npmjs.org/install.sh | sh
+elif [ "$(uname)" = "Darwin" ]; then
+  brew install node
 fi
 
+if [ "$(which wget)"="wget not found" ]; then
+  curl -L "git.io/antigen" -o $HOME/.dotfiles/bin/antigen.zsh && chmod +x $HOME/.dotfiles/bin/antigen.zsh
+  curl -o- "https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh" | bash
+else
+  wget -L "git.io/antigen" -O antigen.zsh && chmod +x antigen.zsh && mv antigen.zsh $HOME/.dotfiles/bin/antigen.zsh
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install node
+npm install -g commitizen cz-conventional-changelog
+echo "\n\e[32m$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -) \e[39m\n" 
 echo "Done!"
 echo "Existing files moved to $HOME/.dotfiles/backup"
+echo "Please restart your shell now"
