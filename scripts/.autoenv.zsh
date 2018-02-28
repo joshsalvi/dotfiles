7469a6f19bde93d67e83aba1afa7ae737e672838
Tim Otlik <t.otlik@vivaldi.net>
2018-02-28 19:44:16 +0100

    edit github_issue script on dir entry a message will be displayed to inform the user about the api call to get current issues
    Also `autoenv_init` now links to ~/.dotfiles/scripts/.autoenv.zsh and .autoenv_leave.zsh instead of copying them

--- scripts/.autoenv.zsh
+++ scripts/.autoenv.zsh
@@ -1,6 +1,10 @@
 if [[ -e ~/.dotfiles/bin/github_issues ]]; then
-	issues=$(~/.dotfiles/bin/github_issues)
-	if (( issues > 0 )); then
-	echo "$(~/.dotfiles/bin/github_issues)" > .issues
+	git_url=$(git remote get-url origin)
+	if [[ -d .git ]] && [[ "$git_url" = *'github.com'* ]]; then
+		echo "Fetching Github Issues..."
+		issues=$(~/.dotfiles/bin/github_issues)
+		if (( issues > 0 )); then
+			echo "$(~/.dotfiles/bin/github_issues)" > .issues
+		fi
 	fi
 fi
